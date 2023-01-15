f26b8ab468ea3a846bbdc21d1dbe54beee4c6723
vhprestes <vhprestes@gmail.com>
2023-01-15 11:38:48 -0300

    feat: dogs css

--- frontend/src/Pages/Dogs.tsx
+++ frontend/src/Pages/Dogs.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import { Box, Button, CssBaseline, Grid } from '@mui/material';
 import { fetchDogs } from '../API/fetchExternalAPI';
+import '../App.css'
 
 interface DogPageState {
   imageUrl: string;
@@ -16,28 +17,83 @@ class DogsPage extends React.Component<{}, DogPageState> {
   }
 
   refreshImage = () => {
-    fetchDogs()
-      .then((imageUrl) => {
-        this.setState({ imageUrl });
-      });
+    fetchDogs().then((imageUrl) => {
+      this.setState({ imageUrl });
+    });
   };
 
   render() {
     // qual o formato da resposta? imagem ou mp4?
     if (this.state.imageUrl.match(/\.mp4$/) != null) {
       return (
-        <div>
-          <Button onClick={this.refreshImage}>Refresh</Button>
-          <video className='xibiu' src={this.state.imageUrl} width={'400px'} />
-        </div>
+        <Grid
+          container
+          component="main"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <CssBaseline />
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Button fullWidth onClick={this.refreshImage}>
+              Refresh
+            </Button>
+            <div className='media'>
+
+            <video
+              className="xibiu"
+              src={this.state.imageUrl}
+              width={'400px'}
+            />
+            </div>
+          </Box>
+        </Grid>
       );
     } else {
       return (
         <div>
-          <Button startIcon color='primary' variant='contained' onClick={this.refreshImage}>Refresh</Button>
-          {this.state.imageUrl ? (
-            <img  className='xibiu' src={this.state.imageUrl} alt="Random dog" width={'400px'}/>
-          ) : null}
+          <Grid
+            container
+            component="main"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <CssBaseline />
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Button
+                fullWidth
+                color="primary"
+                variant="contained"
+                onClick={this.refreshImage}
+              >
+                Refresh
+              </Button>
+              {this.state.imageUrl ? (
+                <div className='media'>
+                <img
+                  src={this.state.imageUrl}
+                  alt="Random dog"
+                  width={'400px'}
+                />
+                </div>
+              ) : null}
+            </Box>
+          </Grid>
         </div>
       );
     }
